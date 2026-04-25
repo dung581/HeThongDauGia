1dc6587d13dc4fc716cd2909b2cb7e085c302042
baocon8527-cell <baocon8527@gmail.com>
2026-04-25 09:28:07 +0700

    fix entities (#15)
    Co-authored-by: Duc Anh <chuducanhnt@outlook.com>

--- src/main/java/Common/DataBase/entities/bid.java
+++ src/main/java/Common/DataBase/entities/bid.java
@@ -1,12 +1,16 @@
 package Common.DataBase.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class bid implements Serializable {
     private long id;
     private long auction_id;
