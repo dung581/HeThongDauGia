767586a0808f56f735332d512ab555e330e375ce
Dung <vuvietdung100@gmail.com>
2026-04-27 09:55:49 +0700

    commit code

--- src/main/java/Client/UILoginController.java
+++ src/main/java/Client/UILoginController.java
@@ -1,7 +1,13 @@
 package Client;
 
 import Common.DataBase.entities.User;
-import Server.service.Exceptions.*;
+import Server.service.Exceptions.DataAccessException;
+import Server.service.Exceptions.PasswordIsBlankException;
+import Server.service.Exceptions.UserNotFoundException;
+import Server.service.Exceptions.UsernameAlreadyExistsException;
+import Server.service.Exceptions.UsernameIsBlankException;
+import Server.service.Exceptions.WrongPasswordException;
+import javafx.animation.PauseTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,6 +17,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import javax.swing.*;
 import java.io.IOException;
@@ -18,7 +25,7 @@ import java.io.IOException;
 public class UILoginController {
     private Parent root;
     private Stage stage;
-    private Authservice authService;
+    private final Authservice authService;
 
     @FXML
     private TextField name;
@@ -32,61 +39,94 @@ public class UILoginController {
     private TextField regpassagain;
     @FXML
     private Label welcomeText;
+    @FXML
+    private Label registerMessage;
 
     public UILoginController() {
         authService = new Authservice();
     }
 
-    // Ham login xu ly dang nhap
     public void login() throws UsernameIsBlankException, UserNotFoundException, WrongPasswordException, PasswordIsBlankException {
         String ten = name.getText();
         String pass = password.getText();
 
         try {
             User user = authService.login(ten, pass);
-            JOptionPane.showMessageDialog(null, "Dang nhap thanh cong: " + user.getUsername(), "Thong bao", JOptionPane.INFORMATION_MESSAGE);
-            // TODO: chuyen sang man hinh menu sau khi dang nhap thanh cong
+            JOptionPane.showMessageDialog(null, "Đăng nhập thành công: " + user.getUsername(), "Thông báo", JOptionPane.INFORMATION_MESSAGE);
         } catch (UsernameIsBlankException e) {
-            JOptionPane.showMessageDialog(null, e.getMessage(), "Thong bao", JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Thông báo", JOptionPane.WARNING_MESSAGE);
         } catch (UserNotFoundException e) {
-            JOptionPane.showMessageDialog(null, e.getMessage(), "Thong bao", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Thông báo", JOptionPane.ERROR_MESSAGE);
         } catch (WrongPasswordException e) {
-            JOptionPane.showMessageDialog(null, e.getMessage(), "Thong bao", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Thông báo", JOptionPane.ERROR_MESSAGE);
+        } catch (DataAccessException e) {
+            JOptionPane.showMessageDialog(null, "Không thể kết nối database. Kiểm tra SQL Server và db.properties.", "Thông báo", JOptionPane.ERROR_MESSAGE);
         }
     }
 
     public void Register(ActionEvent actionEvent) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("/com/template/hellfx/UIRegister.fxml"));
+        switchScene(actionEvent, "/com/template/hellfx/UIRegister.fxml");
+    }
+
+    public void goToLogin(ActionEvent actionEvent) throws IOException {
+        switchScene(actionEvent, "/com/template/hellfx/UILogin.fxml");
+    }
+
+    public void Register2(ActionEvent actionEvent) throws IOException, UsernameIsBlankException, UsernameAlreadyExistsException, PasswordIsBlankException {
+        String tenDK = regname.getText();
+        String mkhau = regpass.getText();
+        String mkhauLai = regpassagain.getText();
+
+        clearRegisterMessage();
+
+        if (!mkhau.equals(mkhauLai)) {
+            JOptionPane.showMessageDialog(null, "Đăng ký thất bại: mật khẩu nhập lại không khớp", "Thông báo", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        try {
+            authService.register(tenDK, mkhau);
+            showRegisterMessage("Đăng ký thành công");
+
+            PauseTransition pause = new PauseTransition(Duration.seconds(1.5));
+            pause.setOnFinished(event -> {
+                try {
+                    switchScene(actionEvent, "/com/template/hellfx/UILogin.fxml");
+                } catch (IOException e) {
+                    JOptionPane.showMessageDialog(null, "Không mở được màn hình đăng nhập.", "Thông báo", JOptionPane.ERROR_MESSAGE);
+                }
+            });
+            pause.play();
+        } catch (UsernameIsBlankException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Thông báo", JOptionPane.WARNING_MESSAGE);
+        } catch (UsernameAlreadyExistsException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Thông báo", JOptionPane.ERROR_MESSAGE);
+        } catch (PasswordIsBlankException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Thông báo", JOptionPane.WARNING_MESSAGE);
+        } catch (DataAccessException e) {
+            JOptionPane.showMessageDialog(null, "Đăng ký thất bại do lỗi kết nối máy chủ.", "Thông báo", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private void switchScene(ActionEvent actionEvent, String fxmlPath) throws IOException {
+        root = FXMLLoader.load(getClass().getResource(fxmlPath));
         stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 
-    public void Register2(ActionEvent actionEvent) throws IOException,UsernameIsBlankException, UsernameAlreadyExistsException, PasswordIsBlankException {
-        String tenDK = regname.getText();
-        String mkhau = regpass.getText();
-        String mkhauLai = regpassagain.getText();
-
-        if (!mkhau.equals(mkhauLai)) {
-            JOptionPane.showMessageDialog(null, "Dang ky that bai: mat khau nhap lai khong khop", "Thong bao", JOptionPane.ERROR_MESSAGE);
-            return;
+    private void showRegisterMessage(String message) {
+        if (registerMessage != null) {
+            registerMessage.setText(message);
+            registerMessage.setVisible(true);
         }
+    }
 
-        try {
-            authService.register(tenDK, mkhau);
-            JOptionPane.showMessageDialog(null, "Dang ky thanh cong", "Thong bao", JOptionPane.INFORMATION_MESSAGE);
-
-            // tro ve man hinh login
-            root = FXMLLoader.load(getClass().getResource("/com/template/hellfx/UILogin.fxml"));
-            stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
-        } catch (UsernameIsBlankException e) {
-            JOptionPane.showMessageDialog(null, e.getMessage(), "Thong bao", JOptionPane.WARNING_MESSAGE);
-        } catch (UsernameAlreadyExistsException e) {
-            JOptionPane.showMessageDialog(null, e.getMessage(), "Thong bao", JOptionPane.ERROR_MESSAGE);
+    private void clearRegisterMessage() {
+        if (registerMessage != null) {
+            registerMessage.setText("");
+            registerMessage.setVisible(false);
         }
     }
 }
