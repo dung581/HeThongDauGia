e1af3ded22d619b84c2c6b1e3d631874e984d438
baocon8527-cell <baocon8527@gmail.com>
2026-05-09 20:05:56 +0700

    postgreql (#24)
    Co-authored-by: Duc Anh <chuducanhnt@outlook.com>

--- src/main/java/Common/DataBase/repository/AccountRepository.java
+++ src/main/java/Common/DataBase/repository/AccountRepository.java
@@ -14,9 +14,11 @@ import java.util.List;
 
 public class AccountRepository {
     DbConnection db= new DbConnection();
+
     public List<Account> getAllAccount() {
         List<Account> accounts = new ArrayList<>();
-        String sql = "SELECT * FROM Account";
+        String sql = "SELECT * FROM account";
+
         try (Connection conn = db.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql);
              ResultSet rs = ps.executeQuery()) {
@@ -29,51 +31,70 @@ public class AccountRepository {
                 a.setLocked_balance(rs.getLong("locked_balance"));
                 accounts.add(a);
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         return accounts;
     }
+
     public void CreateAccount(Account account) {
-        // Khởi tạo ngay trong hàm
-        String sql = "INSERT INTO Account (user_id, balance, locked_balance) VALUES (?, ?, ?)";
+
+        String sql = "INSERT INTO account (user_id, balance, locked_balance) VALUES (?, ?, ?)";
 
         try (Connection conn = db.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
+
             ps.setLong(1, account.getUser_id());
             ps.setLong(2, account.getBalance());
             ps.setLong(3, account.getLocked_balance());
+
             ps.executeUpdate();
-        } catch (Exception e) { e.printStackTrace(); }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
     public Account getAccountByUserId(long user_id) {
+
         String sql = "SELECT * FROM account WHERE user_id = ?";
 
         try (Connection conn = db.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
 
             ps.setLong(1, user_id);
+
             ResultSet rs = ps.executeQuery();
 
             if (rs.next()) {
+
                 Account a = new Account();
+
                 a.setId(rs.getLong("id"));
                 a.setUser_id(rs.getLong("user_id"));
                 a.setBalance(rs.getLong("balance"));
                 a.setLocked_balance(rs.getLong("locked_balance"));
+
                 return a;
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         return null;
     }
+
     public void update(Account a) {
+
         String sql = """
-        UPDATE account
-        SET balance = ?, locked_balance = ?
-        WHERE user_id = ?
-    """;
+            UPDATE account
+            SET balance = ?, locked_balance = ?
+            WHERE user_id = ?
+        """;
+
         try (Connection conn = db.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
 
@@ -91,5 +112,4 @@ public class AccountRepository {
             throw new RuntimeException(e);
         }
     }
-
 }
--- src/main/java/Common/DataBase/repository/RepoUseInService/UserAccountRepository.java
+++ src/main/java/Common/DataBase/repository/RepoUseInService/UserAccountRepository.java
@@ -8,31 +8,45 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class UserAccountRepository {
+
     public UserAccount getUserAccount(long userId) {
+
         String sql = """
-        SELECT u.id, u.fullname, a.balance, a.locked_balance
-        FROM user u
-        JOIN account a ON u.id = a.user_id
-        WHERE u.id = ?
-    """;
-        DbConnection db= new DbConnection();
-        try (Connection conn = db.getConnection();
-             PreparedStatement ps = conn.prepareStatement(sql)) {
+            SELECT u.id,
+                   u.fullname,
+                   a.balance,
+                   a.locked_balance
+            FROM users u
+            JOIN account a ON u.id = a.user_id
+            WHERE u.id = ?
+        """;
+
+        DbConnection db = new DbConnection();
+
+        try (
+                Connection conn = db.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)
+        ) {
 
             ps.setLong(1, userId);
-            ResultSet rs = ps.executeQuery();
 
-            if (rs.next()) {
-                UserAccount ua = new UserAccount();
-                ua.setUserId(rs.getLong("id"));
-                ua.setFullname(rs.getString("fullname"));
-                ua.setBalance(rs.getLong("balance"));
-                ua.setLockedBalance(rs.getLong("locked_balance"));
-                return ua;
+            try (ResultSet rs = ps.executeQuery()) {
+
+                if (rs.next()) {
+
+                    UserAccount ua = new UserAccount();
+
+                    ua.setUserId(rs.getLong("id"));
+                    ua.setFullname(rs.getString("fullname"));
+                    ua.setBalance(rs.getLong("balance"));
+                    ua.setLockedBalance(rs.getLong("locked_balance"));
+
+                    return ua;
+                }
             }
 
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Error while getting user account", e);
         }
 
         return null;
