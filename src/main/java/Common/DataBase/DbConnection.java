5fb7a080f20ed7747bc497e4bb2b0634864b9660
dungne <dungng64207@gmail.com>
2026-05-12 15:11:33 +0700

    doi db moi ne

--- src/main/java/Common/DataBase/DbConnection.java
+++ src/main/java/Common/DataBase/DbConnection.java
@@ -6,12 +6,16 @@ import java.sql.DriverManager;
 public class DbConnection {
 
     private static final String URL =
-            "jdbc:postgresql://aws-1-ap-south-1.pooler.supabase.com:5432/postgres?sslmode=require";
+            "jdbc:postgresql://localhost:5432/auction";
 
-    private static final String USER = "postgres.pqaqflhzgkgqjuoysxuy";
-    private static final String PASSWORD = "baitaplon@@";
+    private static final String USER =
+            "postgres";
+
+    private static final String PASSWORD =
+            "123456";
 
     public static Connection getConnection() throws Exception {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
+
 }
