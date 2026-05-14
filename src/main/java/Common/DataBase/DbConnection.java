32a695f149f3c5a0d4c4b8d6628acc8febbe3ac4
Dung <vuvietdung100@gmail.com>
2026-05-14 21:48:36 +0700

    db

--- src/main/java/Common/DataBase/DbConnection.java
+++ src/main/java/Common/DataBase/DbConnection.java
@@ -1,3 +1,4 @@
+
 package Common.DataBase;
 
 import java.sql.Connection;
@@ -19,3 +20,27 @@ public class DbConnection {
     }
 
 }
+/*
+package Common.DataBase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DbConnection {
+
+    private static final String URL =
+            "jdbc:postgresql://aws-1-ap-south-1.pooler.supabase.com:5432/postgres?sslmode=require";
+
+    private static final String USER =
+            "postgres.pqaqflhzgkgqjuoysxuy";
+
+    private static final String PASSWORD =
+            "baitaplon@@";
+
+    public static Connection getConnection() throws Exception {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+}
+
+ */
